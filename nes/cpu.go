c4f5aa2d3ef75282ee0d2733f5f5dddf9097fcf1
Michael Fogleman <fogleman@gmail.com>
2015-03-04 20:39:13 -0500

    cpu wip

--- nes/cpu.go
+++ nes/cpu.go
@@ -1,6 +1,9 @@
 package nes
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
 // Addressing Modes
 const (
@@ -20,7 +23,42 @@ const (
 	ZERO_PAGE_Y
 )
 
-var InstructionMode = [256]byte{
+var InstructionNames = [256]string{
+	"BRK", "ORA", "???", "???", "???", "ORA", "ASL", "???",
+	"PHP", "ORA", "ASL", "???", "???", "ORA", "ASL", "???",
+	"BPL", "ORA", "???", "???", "???", "ORA", "ASL", "???",
+	"CLC", "ORA", "???", "???", "???", "ORA", "ASL", "???",
+	"JSR", "AND", "???", "???", "BIT", "AND", "ROL", "???",
+	"PLP", "AND", "ROL", "???", "BIT", "AND", "ROL", "???",
+	"BMI", "AND", "???", "???", "???", "AND", "ROL", "???",
+	"SEC", "AND", "???", "???", "???", "AND", "ROL", "???",
+	"RTI", "EOR", "???", "???", "???", "EOR", "LSR", "???",
+	"PHA", "EOR", "LSR", "???", "JMP", "EOR", "LSR", "???",
+	"BVC", "EOR", "???", "???", "???", "EOR", "LSR", "???",
+	"CLI", "EOR", "???", "???", "???", "EOR", "LSR", "???",
+	"RTS", "ADC", "???", "???", "???", "ADC", "ROR", "???",
+	"PLA", "ADC", "ROR", "???", "JMP", "ADC", "ROR", "???",
+	"BVS", "ADC", "???", "???", "???", "ADC", "ROR", "???",
+	"SEI", "ADC", "???", "???", "???", "ADC", "ROR", "???",
+	"???", "STA", "???", "???", "STY", "STA", "STX", "???",
+	"DEY", "???", "TXA", "???", "STY", "STA", "STX", "???",
+	"BCC", "STA", "???", "???", "STY", "STA", "STX", "???",
+	"TYA", "STA", "TXS", "???", "???", "STA", "???", "???",
+	"LDY", "LDA", "LDX", "???", "LDY", "LDA", "LDX", "???",
+	"TAY", "LDA", "TAX", "???", "LDY", "LDA", "LDX", "???",
+	"BCS", "LDA", "???", "???", "LDY", "LDA", "LDX", "???",
+	"CLV", "LDA", "TSX", "???", "LDY", "LDA", "LDX", "???",
+	"CPY", "CMP", "???", "???", "CPY", "CMP", "DEC", "???",
+	"INY", "CMP", "DEX", "???", "CPY", "CMP", "DEC", "???",
+	"BNE", "CMP", "???", "???", "???", "CMP", "DEC", "???",
+	"CLD", "CMP", "???", "???", "???", "CMP", "DEC", "???",
+	"CPX", "SBC", "???", "???", "CPX", "SBC", "INC", "???",
+	"INX", "SBC", "NOP", "???", "CPX", "SBC", "INC", "???",
+	"BEQ", "SBC", "???", "???", "???", "SBC", "INC", "???",
+	"SED", "SBC", "???", "???", "???", "SBC", "INC", "???",
+}
+
+var InstructionModes = [256]byte{
 	0x6, 0x7, 0x0, 0x0, 0x0, 0xb, 0xb, 0x0, 0x6, 0x5, 0x4, 0x0, 0x0, 0x1, 0x1, 0x0,
 	0xa, 0x9, 0x0, 0x0, 0x0, 0xc, 0xc, 0x0, 0x6, 0x3, 0x0, 0x0, 0x0, 0x2, 0x2, 0x0,
 	0x1, 0x7, 0x0, 0x0, 0xb, 0xb, 0xb, 0x0, 0x6, 0x5, 0x4, 0x0, 0x1, 0x1, 0x1, 0x0,
@@ -82,7 +120,8 @@ func PageCrossed(a, b uint16) bool {
 }
 
 type CPU struct {
-	Memory        // memory interface
+	Memory // memory interface
+	Table  [256]func(uint16)
 	Cycles uint64 // number of cycles
 	PC     uint16 // program counter
 	SP     byte   // stack pointer
@@ -100,13 +139,51 @@ type CPU struct {
 
 func NewCPU(memory Memory) *CPU {
 	cpu := CPU{Memory: memory}
+	cpu.Table = [256]func(uint16){
+		cpu.BRK, cpu.ORA, cpu.UNK, cpu.UNK, cpu.UNK, cpu.ORA, cpu.ASL, cpu.UNK,
+		cpu.PHP, cpu.ORA, cpu.ASL, cpu.UNK, cpu.UNK, cpu.ORA, cpu.ASL, cpu.UNK,
+		cpu.BPL, cpu.ORA, cpu.UNK, cpu.UNK, cpu.UNK, cpu.ORA, cpu.ASL, cpu.UNK,
+		cpu.CLC, cpu.ORA, cpu.UNK, cpu.UNK, cpu.UNK, cpu.ORA, cpu.ASL, cpu.UNK,
+		cpu.JSR, cpu.AND, cpu.UNK, cpu.UNK, cpu.BIT, cpu.AND, cpu.ROL, cpu.UNK,
+		cpu.PLP, cpu.AND, cpu.ROL, cpu.UNK, cpu.BIT, cpu.AND, cpu.ROL, cpu.UNK,
+		cpu.BMI, cpu.AND, cpu.UNK, cpu.UNK, cpu.UNK, cpu.AND, cpu.ROL, cpu.UNK,
+		cpu.SEC, cpu.AND, cpu.UNK, cpu.UNK, cpu.UNK, cpu.AND, cpu.ROL, cpu.UNK,
+		cpu.RTI, cpu.EOR, cpu.UNK, cpu.UNK, cpu.UNK, cpu.EOR, cpu.LSR, cpu.UNK,
+		cpu.PHA, cpu.EOR, cpu.LSR, cpu.UNK, cpu.JMP, cpu.EOR, cpu.LSR, cpu.UNK,
+		cpu.BVC, cpu.EOR, cpu.UNK, cpu.UNK, cpu.UNK, cpu.EOR, cpu.LSR, cpu.UNK,
+		cpu.CLI, cpu.EOR, cpu.UNK, cpu.UNK, cpu.UNK, cpu.EOR, cpu.LSR, cpu.UNK,
+		cpu.RTS, cpu.ADC, cpu.UNK, cpu.UNK, cpu.UNK, cpu.ADC, cpu.ROR, cpu.UNK,
+		cpu.PLA, cpu.ADC, cpu.ROR, cpu.UNK, cpu.JMP, cpu.ADC, cpu.ROR, cpu.UNK,
+		cpu.BVS, cpu.ADC, cpu.UNK, cpu.UNK, cpu.UNK, cpu.ADC, cpu.ROR, cpu.UNK,
+		cpu.SEI, cpu.ADC, cpu.UNK, cpu.UNK, cpu.UNK, cpu.ADC, cpu.ROR, cpu.UNK,
+		cpu.UNK, cpu.STA, cpu.UNK, cpu.UNK, cpu.STY, cpu.STA, cpu.STX, cpu.UNK,
+		cpu.DEY, cpu.UNK, cpu.TXA, cpu.UNK, cpu.STY, cpu.STA, cpu.STX, cpu.UNK,
+		cpu.BCC, cpu.STA, cpu.UNK, cpu.UNK, cpu.STY, cpu.STA, cpu.STX, cpu.UNK,
+		cpu.TYA, cpu.STA, cpu.TXS, cpu.UNK, cpu.UNK, cpu.STA, cpu.UNK, cpu.UNK,
+		cpu.LDY, cpu.LDA, cpu.LDX, cpu.UNK, cpu.LDY, cpu.LDA, cpu.LDX, cpu.UNK,
+		cpu.TAY, cpu.LDA, cpu.TAX, cpu.UNK, cpu.LDY, cpu.LDA, cpu.LDX, cpu.UNK,
+		cpu.BCS, cpu.LDA, cpu.UNK, cpu.UNK, cpu.LDY, cpu.LDA, cpu.LDX, cpu.UNK,
+		cpu.CLV, cpu.LDA, cpu.TSX, cpu.UNK, cpu.LDY, cpu.LDA, cpu.LDX, cpu.UNK,
+		cpu.CPY, cpu.CMP, cpu.UNK, cpu.UNK, cpu.CPY, cpu.CMP, cpu.DEC, cpu.UNK,
+		cpu.INY, cpu.CMP, cpu.DEX, cpu.UNK, cpu.CPY, cpu.CMP, cpu.DEC, cpu.UNK,
+		cpu.BNE, cpu.CMP, cpu.UNK, cpu.UNK, cpu.UNK, cpu.CMP, cpu.DEC, cpu.UNK,
+		cpu.CLD, cpu.CMP, cpu.UNK, cpu.UNK, cpu.UNK, cpu.CMP, cpu.DEC, cpu.UNK,
+		cpu.CPX, cpu.SBC, cpu.UNK, cpu.UNK, cpu.CPX, cpu.SBC, cpu.INC, cpu.UNK,
+		cpu.INX, cpu.SBC, cpu.NOP, cpu.UNK, cpu.CPX, cpu.SBC, cpu.INC, cpu.UNK,
+		cpu.BEQ, cpu.SBC, cpu.UNK, cpu.UNK, cpu.UNK, cpu.SBC, cpu.INC, cpu.UNK,
+		cpu.SED, cpu.SBC, cpu.UNK, cpu.UNK, cpu.UNK, cpu.SBC, cpu.INC, cpu.UNK,
+	}
 	cpu.Reset()
 	return &cpu
 }
 
 func (cpu *CPU) Reset() {
+	fmt.Printf("%T\n", cpu.ADC)
 	cpu.Cycles = 0
-	cpu.PC = cpu.Read16(0xFFFC)
+	// cpu.PC = cpu.Read16(0xFFFC)
+	cpu.PC = 0xC000
+	cpu.SP = 0xFD
+	cpu.SetFlags(0x24)
 }
 
 // Flag Functions
@@ -118,6 +195,7 @@ func (cpu *CPU) Flags() byte {
 	flags |= cpu.I << 2
 	flags |= cpu.D << 3
 	flags |= cpu.B << 4
+	flags |= 1 << 5
 	flags |= cpu.V << 6
 	flags |= cpu.N << 7
 	return flags
@@ -149,41 +227,33 @@ func (cpu *CPU) SetN(value byte) {
 	}
 }
 
-// Instructions
-
-func (cpu *CPU) ADC(address uint16) {
-}
-
-func (cpu *CPU) CLD() {
-	cpu.D = 0
-}
-
-func (cpu *CPU) LDA(address uint16) {
-	cpu.A = cpu.Read(address)
-	cpu.SetZ(cpu.A)
-	cpu.SetN(cpu.A)
-}
-
-func (cpu *CPU) LDX(address uint16) {
-	cpu.X = cpu.Read(address)
-	cpu.SetZ(cpu.X)
-	cpu.SetN(cpu.X)
-}
-
-func (cpu *CPU) SEI() {
-	cpu.I = 1
-}
-
-func (cpu *CPU) TXS() {
-	cpu.SP = cpu.X
-}
-
 // Step
 
-func (cpu *CPU) Step() {
-	fmt.Println(cpu)
+func (cpu *CPU) PrintInstruction() {
 	opcode := cpu.Read(cpu.PC)
-	mode := InstructionMode[opcode]
+	bytes := InstructionBytes[opcode]
+	name := InstructionNames[opcode]
+	w0 := fmt.Sprintf("%02X", cpu.Read(cpu.PC+0))
+	w1 := fmt.Sprintf("%02X", cpu.Read(cpu.PC+1))
+	w2 := fmt.Sprintf("%02X", cpu.Read(cpu.PC+2))
+	if bytes < 2 {
+		w1 = "  "
+	}
+	if bytes < 3 {
+		w2 = "  "
+	}
+	fmt.Printf(
+		"%4X  %s %s %s  %s %27s"+
+			"A:%02X X:%02X Y:%02X P:%02X SP:%02X CYC:%3d\n",
+		cpu.PC, w0, w1, w2, name, "",
+		cpu.A, cpu.X, cpu.Y, cpu.Flags(), cpu.SP, cpu.Cycles*3)
+}
+
+func (cpu *CPU) Step() {
+	cpu.PrintInstruction()
+
+	opcode := cpu.Read(cpu.PC)
+	mode := InstructionModes[opcode]
 
 	var address uint16
 	// var pageCrossed bool
@@ -229,21 +299,242 @@ func (cpu *CPU) Step() {
 	}
 
 	cpu.PC += uint16(InstructionBytes[opcode])
+	cpu.Cycles += uint64(InstructionCycles[opcode])
 
-	switch opcode {
-	case 0x69:
-		cpu.ADC(address)
-	case 0x78:
-		cpu.SEI()
-	case 0x9A:
-		cpu.TXS()
-	case 0xA2:
-		cpu.LDX(address)
-	case 0xAD:
-		cpu.LDA(address)
-	case 0xD8:
-		cpu.CLD()
-	default:
-		fmt.Printf("Unrecognized opcode: 0x%02x\n", opcode)
-	}
+	cpu.Table[opcode](address)
+}
+
+// Instructions
+func (cpu *CPU) ADC(address uint16) {
+	log.Fatalln("Unimplemented instruction: ADC")
+}
+
+func (cpu *CPU) AND(address uint16) {
+	log.Fatalln("Unimplemented instruction: AND")
+}
+
+func (cpu *CPU) ASL(address uint16) {
+	log.Fatalln("Unimplemented instruction: ASL")
+}
+
+func (cpu *CPU) BCC(address uint16) {
+	log.Fatalln("Unimplemented instruction: BCC")
+}
+
+func (cpu *CPU) BCS(address uint16) {
+	log.Fatalln("Unimplemented instruction: BCS")
+}
+
+func (cpu *CPU) BEQ(address uint16) {
+	log.Fatalln("Unimplemented instruction: BEQ")
+}
+
+func (cpu *CPU) BIT(address uint16) {
+	log.Fatalln("Unimplemented instruction: BIT")
+}
+
+func (cpu *CPU) BMI(address uint16) {
+	log.Fatalln("Unimplemented instruction: BMI")
+}
+
+func (cpu *CPU) BNE(address uint16) {
+	log.Fatalln("Unimplemented instruction: BNE")
+}
+
+func (cpu *CPU) BPL(address uint16) {
+	log.Fatalln("Unimplemented instruction: BPL")
+}
+
+func (cpu *CPU) BRK(address uint16) {
+	log.Fatalln("Unimplemented instruction: BRK")
+}
+
+func (cpu *CPU) BVC(address uint16) {
+	log.Fatalln("Unimplemented instruction: BVC")
+}
+
+func (cpu *CPU) BVS(address uint16) {
+	log.Fatalln("Unimplemented instruction: BVS")
+}
+
+func (cpu *CPU) CLC(address uint16) {
+	log.Fatalln("Unimplemented instruction: CLC")
+}
+
+func (cpu *CPU) CLD(address uint16) {
+	cpu.D = 0
+}
+
+func (cpu *CPU) CLI(address uint16) {
+	log.Fatalln("Unimplemented instruction: CLI")
+}
+
+func (cpu *CPU) CLV(address uint16) {
+	log.Fatalln("Unimplemented instruction: CLV")
+}
+
+func (cpu *CPU) CMP(address uint16) {
+	log.Fatalln("Unimplemented instruction: CMP")
+}
+
+func (cpu *CPU) CPX(address uint16) {
+	log.Fatalln("Unimplemented instruction: CPX")
+}
+
+func (cpu *CPU) CPY(address uint16) {
+	log.Fatalln("Unimplemented instruction: CPY")
+}
+
+func (cpu *CPU) DEC(address uint16) {
+	log.Fatalln("Unimplemented instruction: DEC")
+}
+
+func (cpu *CPU) DEX(address uint16) {
+	log.Fatalln("Unimplemented instruction: DEX")
+}
+
+func (cpu *CPU) DEY(address uint16) {
+	log.Fatalln("Unimplemented instruction: DEY")
+}
+
+func (cpu *CPU) EOR(address uint16) {
+	log.Fatalln("Unimplemented instruction: EOR")
+}
+
+func (cpu *CPU) INC(address uint16) {
+	log.Fatalln("Unimplemented instruction: INC")
+}
+
+func (cpu *CPU) INX(address uint16) {
+	log.Fatalln("Unimplemented instruction: INX")
+}
+
+func (cpu *CPU) INY(address uint16) {
+	log.Fatalln("Unimplemented instruction: INY")
+}
+
+func (cpu *CPU) JMP(address uint16) {
+	cpu.PC = address
+}
+
+func (cpu *CPU) JSR(address uint16) {
+	log.Fatalln("Unimplemented instruction: JSR")
+}
+
+func (cpu *CPU) LDA(address uint16) {
+	cpu.A = cpu.Read(address)
+	cpu.SetZ(cpu.A)
+	cpu.SetN(cpu.A)
+}
+
+func (cpu *CPU) LDX(address uint16) {
+	cpu.X = cpu.Read(address)
+	cpu.SetZ(cpu.X)
+	cpu.SetN(cpu.X)
+}
+
+func (cpu *CPU) LDY(address uint16) {
+	cpu.Y = cpu.Read(address)
+	cpu.SetZ(cpu.Y)
+	cpu.SetN(cpu.Y)
+}
+
+func (cpu *CPU) LSR(address uint16) {
+	log.Fatalln("Unimplemented instruction: LSR")
+}
+
+func (cpu *CPU) NOP(address uint16) {
+	log.Fatalln("Unimplemented instruction: NOP")
+}
+
+func (cpu *CPU) ORA(address uint16) {
+	log.Fatalln("Unimplemented instruction: ORA")
+}
+
+func (cpu *CPU) PHA(address uint16) {
+	log.Fatalln("Unimplemented instruction: PHA")
+}
+
+func (cpu *CPU) PHP(address uint16) {
+	log.Fatalln("Unimplemented instruction: PHP")
+}
+
+func (cpu *CPU) PLA(address uint16) {
+	log.Fatalln("Unimplemented instruction: PLA")
+}
+
+func (cpu *CPU) PLP(address uint16) {
+	log.Fatalln("Unimplemented instruction: PLP")
+}
+
+func (cpu *CPU) ROL(address uint16) {
+	log.Fatalln("Unimplemented instruction: ROL")
+}
+
+func (cpu *CPU) ROR(address uint16) {
+	log.Fatalln("Unimplemented instruction: ROR")
+}
+
+func (cpu *CPU) RTI(address uint16) {
+	log.Fatalln("Unimplemented instruction: RTI")
+}
+
+func (cpu *CPU) RTS(address uint16) {
+	log.Fatalln("Unimplemented instruction: RTS")
+}
+
+func (cpu *CPU) SBC(address uint16) {
+	log.Fatalln("Unimplemented instruction: SBC")
+}
+
+func (cpu *CPU) SEC(address uint16) {
+	log.Fatalln("Unimplemented instruction: SEC")
+}
+
+func (cpu *CPU) SED(address uint16) {
+	log.Fatalln("Unimplemented instruction: SED")
+}
+
+func (cpu *CPU) SEI(address uint16) {
+	cpu.I = 1
+}
+
+func (cpu *CPU) STA(address uint16) {
+	log.Fatalln("Unimplemented instruction: STA")
+}
+
+func (cpu *CPU) STX(address uint16) {
+	cpu.Write(address, cpu.X)
+}
+
+func (cpu *CPU) STY(address uint16) {
+	log.Fatalln("Unimplemented instruction: STY")
+}
+
+func (cpu *CPU) TAX(address uint16) {
+	log.Fatalln("Unimplemented instruction: TAX")
+}
+
+func (cpu *CPU) TAY(address uint16) {
+	log.Fatalln("Unimplemented instruction: TAY")
+}
+
+func (cpu *CPU) TSX(address uint16) {
+	log.Fatalln("Unimplemented instruction: TSX")
+}
+
+func (cpu *CPU) TXA(address uint16) {
+	log.Fatalln("Unimplemented instruction: TXA")
+}
+
+func (cpu *CPU) TXS(address uint16) {
+	cpu.SP = cpu.X
+}
+
+func (cpu *CPU) TYA(address uint16) {
+	log.Fatalln("Unimplemented instruction: TYA")
+}
+
+func (cpu *CPU) UNK(address uint16) {
+	log.Fatalln("Unimplemented instruction: UNK")
 }
