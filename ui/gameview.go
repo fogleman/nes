7dfff4182af6477e338db8b422493b707d164e3f
Michael Fogleman <fogleman@gmail.com>
2015-04-01 14:48:05 -0400

    refactor

--- ui/gameview.go
+++ ui/gameview.go
@@ -72,49 +72,6 @@ func drawBuffer(window *glfw.Window) {
 	gl.End()
 }
 
-func readKey(window *glfw.Window, key glfw.Key) bool {
-	return window.GetKey(key) == glfw.Press
-}
-
-func readKeys(window *glfw.Window, turbo bool) [8]bool {
-	var result [8]bool
-	result[nes.ButtonA] = readKey(window, glfw.KeyZ) || (turbo && readKey(window, glfw.KeyA))
-	result[nes.ButtonB] = readKey(window, glfw.KeyX) || (turbo && readKey(window, glfw.KeyS))
-	result[nes.ButtonSelect] = readKey(window, glfw.KeyRightShift)
-	result[nes.ButtonStart] = readKey(window, glfw.KeyEnter)
-	result[nes.ButtonUp] = readKey(window, glfw.KeyUp)
-	result[nes.ButtonDown] = readKey(window, glfw.KeyDown)
-	result[nes.ButtonLeft] = readKey(window, glfw.KeyLeft)
-	result[nes.ButtonRight] = readKey(window, glfw.KeyRight)
-	return result
-}
-
-func readJoystick(joy glfw.Joystick, turbo bool) [8]bool {
-	var result [8]bool
-	if !glfw.JoystickPresent(joy) {
-		return result
-	}
-	axes := glfw.GetJoystickAxes(joy)
-	buttons := glfw.GetJoystickButtons(joy)
-	result[nes.ButtonA] = buttons[0] == 1 || (turbo && buttons[2] == 1)
-	result[nes.ButtonB] = buttons[1] == 1 || (turbo && buttons[3] == 1)
-	result[nes.ButtonSelect] = buttons[6] == 1
-	result[nes.ButtonStart] = buttons[7] == 1
-	result[nes.ButtonUp] = axes[1] < -0.5
-	result[nes.ButtonDown] = axes[1] > 0.5
-	result[nes.ButtonLeft] = axes[0] < -0.5
-	result[nes.ButtonRight] = axes[0] > 0.5
-	return result
-}
-
-func combineButtons(a, b [8]bool) [8]bool {
-	var result [8]bool
-	for i := 0; i < 8; i++ {
-		result[i] = a[i] || b[i]
-	}
-	return result
-}
-
 func updateControllers(window *glfw.Window, console *nes.Console) {
 	turbo := console.PPU.Frame%6 < 3
 	k1 := readKeys(window, turbo)
--- ui/util.go
+++ ui/util.go
@@ -9,8 +9,53 @@ import (
 	"io/ioutil"
 	"os"
 
+	"github.com/fogleman/nes/nes"
 	"github.com/go-gl/gl/v2.1/gl"
+	"github.com/go-gl/glfw/v3.1/glfw"
 )
+
+func readKey(window *glfw.Window, key glfw.Key) bool {
+	return window.GetKey(key) == glfw.Press
+}
+
+func readKeys(window *glfw.Window, turbo bool) [8]bool {
+	var result [8]bool
+	result[nes.ButtonA] = readKey(window, glfw.KeyZ) || (turbo && readKey(window, glfw.KeyA))
+	result[nes.ButtonB] = readKey(window, glfw.KeyX) || (turbo && readKey(window, glfw.KeyS))
+	result[nes.ButtonSelect] = readKey(window, glfw.KeyRightShift)
+	result[nes.ButtonStart] = readKey(window, glfw.KeyEnter)
+	result[nes.ButtonUp] = readKey(window, glfw.KeyUp)
+	result[nes.ButtonDown] = readKey(window, glfw.KeyDown)
+	result[nes.ButtonLeft] = readKey(window, glfw.KeyLeft)
+	result[nes.ButtonRight] = readKey(window, glfw.KeyRight)
+	return result
+}
+
+func readJoystick(joy glfw.Joystick, turbo bool) [8]bool {
+	var result [8]bool
+	if !glfw.JoystickPresent(joy) {
+		return result
+	}
+	axes := glfw.GetJoystickAxes(joy)
+	buttons := glfw.GetJoystickButtons(joy)
+	result[nes.ButtonA] = buttons[0] == 1 || (turbo && buttons[2] == 1)
+	result[nes.ButtonB] = buttons[1] == 1 || (turbo && buttons[3] == 1)
+	result[nes.ButtonSelect] = buttons[6] == 1
+	result[nes.ButtonStart] = buttons[7] == 1
+	result[nes.ButtonUp] = axes[1] < -0.5
+	result[nes.ButtonDown] = axes[1] > 0.5
+	result[nes.ButtonLeft] = axes[0] < -0.5
+	result[nes.ButtonRight] = axes[0] > 0.5
+	return result
+}
+
+func combineButtons(a, b [8]bool) [8]bool {
+	var result [8]bool
+	for i := 0; i < 8; i++ {
+		result[i] = a[i] || b[i]
+	}
+	return result
+}
 
 func hashFile(path string) (string, error) {
 	data, err := ioutil.ReadFile(path)
